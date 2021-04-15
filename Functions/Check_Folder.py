c3802aa598aa47479eae173ea9742ec2612d51e2
Gold_Cattleman <philippealexandremunch@gmail.com>
2021-04-15 14:49:06 +0200

    Add files via upload

--- Functions/Check_Folder.py
+++ Functions/Check_Folder.py
@@ -1,15 +1,45 @@
 import os
 
 
-def check_folder_video():
-    if os.path.exists("./Video/"):
+def check_folder_video(path=""):
+    if os.path.exists(path + "/Video/"):
         pass
     else:
-        os.mkdir("./Video/")
+        os.mkdir(path + "/Video/")
 
 
-def check_folder_mp3():
-    if os.path.exists("./mp3/"):
+def check_folder_mp3(path=""):
+    if os.path.exists(path + "/mp3/"):
         pass
     else:
-        os.mkdir("./mp3/")
+        os.mkdir(path + "/mp3/")
+
+
+def check_folder_tmp():
+    if os.path.exists("./tmp/"):
+        pass
+    else:
+        os.mkdir("./tmp/")
+
+
+def check_folder_config():
+    if os.path.exists("./Config/"):
+        pass
+    else:
+        os.mkdir("./Config/")
+
+
+def check_folder_up():
+    if os.path.exists("./Config/UserProfile/"):
+        pass
+    else:
+        os.mkdir("./Config/UserProfile/")
+
+
+def main(path=""):
+    check_folder_video(path)
+    check_folder_video(path)
+    check_folder_mp3(path)
+    check_folder_tmp()
+    check_folder_config()
+    check_folder_up()
--- YBDI.py
+++ YBDI.py
@@ -1,16 +1,20 @@
 import os
 import shutil
+import tkinter.messagebox
 from tkinter import *
-
+import webbrowser
 import youtube_dl
 from PIL import ImageTk, Image
-
+from tkinter import filedialog
 from Functions import Check_Folder
 from Functions import Get_Miniature_From_Url
 from Functions import RegexSpecC
 
-
 # TODO  : Clean tmp folder when exit programme
+
+
+version = "Version: 1.2"
+
 
 # Relative Path for Pyinstaller
 def resource_path(relative_path):
@@ -24,7 +28,43 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+def exit_me():
+    master.destroy()
+
+
+def save_exportation_path():
+    global folder_path
+
+    path_file = open("Config\\UserProfile\\ExportPath.ydbi", "w")
+    folder_name = filedialog.askdirectory()
+    folder_path.set(folder_name)
+    path_file.write(folder_name)
+    Check_Folder.main(folder_name)
+
+
+def get_exportation_path():
+    if os.path.exists("Config\\UserProfile\\ExportPath.ydbi"):
+        path = open("Config\\UserProfile\\ExportPath.ydbi", "r")
+        read_path = path.read()
+        return read_path
+
+
+def exportation_information_path():
+    tkinter.messagebox.showinfo(title="Exportation Path", message=get_exportation_path())
+
+
+def about():
+    tkinter.messagebox.showinfo(title="About", message="Developed By GOLD_C4TTL3M@N\n" + version)
+
+
+def read_me():
+    webbrowser.open("https://github.com/PA-R29sY2F0bG1hbg/YBDI/blob/main/README.md")
+
+
 def download_mp3():
+
+
+
     get_url = url.get()
 
     ydl_opts = {
@@ -63,100 +103,201 @@ def download_mp3():
     mp3_id = video['id']
     n_title = RegexSpecC.extract_word(mp3_title)
 
-    for file in os.listdir("."):
+    if os.path.exists("Config\\UserProfile\\ExportPath.ydbi"):
+        for file in os.listdir("."):
 
-        # Webm Condition
-        if file.endswith(".webm"):
-            try:
+            # Webm Condition
+            if file.endswith(".webm"):
 
-                shutil.copy(file, "./mp3/" + n_title + ".mp3")
+                try:
 
-            except OSError:
-                pass
+                    shutil.copy(file, get_exportation_path() + "\\mp3\\" + n_title + ".mp3")
 
-            if os.path.exists("./mp3/" + n_title + ".mp3"):
+                except OSError:
+                    pass
 
-                # state
-                state = Label(master, text="State:", bg="white")
-                state.place(x=175, y=250)
-                success = Label(master, text="Mp3 Downloaded          ", fg="green3", bg="white")
-                success.place(x=210, y=250)
+                if os.path.exists(get_exportation_path() + "\\mp3\\" + n_title + ".mp3"):
 
-                os.remove(file)
+                    # state
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    success = Label(master, text="Mp3 Downloaded          ", fg="green3", bg="white")
+                    success.place(x=210, y=250)
 
-            else:
-                # state
-                state = Label(master, text="State:", bg="white")
-                state.place(x=175, y=250)
-                success = Label(master, text="Error During Download webm          ", fg="Red3", bg="white")
-                success.place(x=210, y=250)
+                    os.remove(file)
 
-        # m4a Condition
-        elif file.endswith(".m4a"):
-            try:
-                shutil.copy(file, "./mp3/" + n_title + ".mp3")
+                else:
+                    # state
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    success = Label(master, text="Error During Download webm          ", fg="Red3", bg="white")
+                    success.place(x=210, y=250)
 
-            except OSError:
-                pass
+            # m4a Condition
+            elif file.endswith(".m4a"):
+                try:
+                    shutil.copy(file, get_exportation_path() + "\\mp3\\" + n_title + ".mp3")
 
-            if os.path.exists("./mp3/" + n_title + ".mp3"):
+                except OSError:
+                    pass
 
-                state = Label(master, text="State:", bg="white")
-                state.place(x=175, y=250)
-                succ = Label(master, text="Mp3 Downloaded          ", fg="green3", bg="white")
-                succ.place(x=210, y=250)
+                if os.path.exists(get_exportation_path() + "\\mp3\\" + n_title + ".mp3"):
 
-                os.remove(file)
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    succ = Label(master, text="Mp3 Downloaded          ", fg="green3", bg="white")
+                    succ.place(x=210, y=250)
 
-            else:
-                # state
-                state = Label(master, text="State:", bg="white")
-                state.place(x=175, y=250)
-                err = Label(master, text="Error During Download m4a          ", fg="Red3", bg="white")
-                err.place(x=210, y=250)
+                    os.remove(file)
+
+                else:
+                    # state
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    err = Label(master, text="Error During Download m4a          ", fg="Red3", bg="white")
+                    err.place(x=210, y=250)
+
+    else:
+        for file in os.listdir("."):
+
+            # Webm Condition
+            if file.endswith(".webm"):
+
+                try:
+
+                    shutil.copy(file, ".\\mp3\\" + n_title + ".mp3")
+
+                except OSError:
+                    pass
+
+                if os.path.exists(".\\mp3\\" + n_title + ".mp3"):
+
+                    # state
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    success = Label(master, text="Mp3 Downloaded          ", fg="green3", bg="white")
+                    success.place(x=210, y=250)
+
+                    os.remove(file)
+
+                else:
+                    # state
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    success = Label(master, text="Error During Download webm          ", fg="Red3", bg="white")
+                    success.place(x=210, y=250)
+
+            # m4a Condition
+            elif file.endswith(".m4a"):
+                try:
+                    shutil.copy(file, ".\\mp3\\" + n_title + ".mp3")
+
+                except OSError:
+                    pass
+
+                if os.path.exists(".\\mp3\\" + n_title + ".mp3"):
+
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    succ = Label(master, text="Mp3 Downloaded          ", fg="green3", bg="white")
+                    succ.place(x=210, y=250)
+
+                    os.remove(file)
+
+                else:
+                    # state
+                    state = Label(master, text="State:", bg="white")
+                    state.place(x=175, y=250)
+                    err = Label(master, text="Error During Download m4a          ", fg="Red3", bg="white")
+                    err.place(x=210, y=250)
 
 
 def download_video():
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+    loading = "#"
 
-    with ydl:
-        result = ydl.extract_info(
-            '%s' % getting_url(),
-            download=True  # We just want to extract the info
-        )
+    if os.path.exists("Config\\UserProfile\\ExportPath.ydbi"):
+        ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 
-    if 'entries' in result:
-        # Can be a playlist or a list of videos
-        video = result['entries'][0]
+        with ydl:
+            result = ydl.extract_info(
+                '%s' % getting_url(),
+                download=True  # We just want to extract the info
+            )
 
-    else:
-        # Just a video
-        video = result
-
-    id = video['id']
-    video_title = video['title']
-    n_title = RegexSpecC.extract_word(video_title)
-
-    if os.path.exists(id + ".mp4"):
-        shutil.copy(id + ".mp4", "./Video/" + n_title + ".mp4")
-
-        if os.path.exists("./Video/" + n_title + ".mp4"):
-            # state
-            state = Label(master, text="State:", bg="white")
-            state.place(x=175, y=250)
-            succ = Label(master, text="Video Downloaded          ", fg="green3", bg="white")
-            succ.place(x=210, y=250)
-
-            os.remove('./' + str(id) + ".mp4")
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
 
         else:
-            # state
-            state = Label(master, text="State:", bg="white")
-            state.place(x=175, y=250)
-            err = Label(master, text="Error During Download video          ", fg="Red3", bg="white")
-            err.place(x=210, y=250)
+            # Just a video
+            video = result
+
+        id = video['id']
+        video_title = video['title']
+        n_title = RegexSpecC.extract_word(video_title)
+
+        if os.path.exists(id + ".mp4"):
+            shutil.copy(id + ".mp4", get_exportation_path() + "\\Video\\" + n_title + ".mp4")
+
+            if os.path.exists(get_exportation_path() + "\\Video\\" + n_title + ".mp4"):
+                # state
+                state = Label(master, text="State:", bg="white")
+                state.place(x=175, y=250)
+                succ = Label(master, text="Video Downloaded          ", fg="green3", bg="white")
+                succ.place(x=210, y=250)
+
+                os.remove('./' + str(id) + ".mp4")
+
+            else:
+                # state
+                state = Label(master, text="State:", bg="white")
+                state.place(x=175, y=250)
+                err = Label(master, text="Error During Download video          ", fg="Red3", bg="white")
+                err.place(x=210, y=250)
+        else:
+            print("File not exists")
+
     else:
-        print("File not exists")
+        ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+
+        with ydl:
+            result = ydl.extract_info(
+                '%s' % getting_url(),
+                download=True  # We just want to extract the info
+            )
+
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
+
+        else:
+            # Just a video
+            video = result
+
+        id = video['id']
+        video_title = video['title']
+        n_title = RegexSpecC.extract_word(video_title)
+
+        if os.path.exists(id + ".mp4"):
+            shutil.copy(id + ".mp4", ".\\video\\" + n_title + ".mp4")
+
+            if os.path.exists(".\\video\\" + n_title + ".mp4"):
+                # state
+                state = Label(master, text="State:", bg="white")
+                state.place(x=175, y=250)
+                succ = Label(master, text="Video Downloaded          ", fg="green3", bg="white")
+                succ.place(x=210, y=250)
+
+                os.remove('./' + str(id) + ".mp4")
+
+            else:
+                # state
+                state = Label(master, text="State:", bg="white")
+                state.place(x=175, y=250)
+                err = Label(master, text="Error During Download video          ", fg="Red3", bg="white")
+                err.place(x=210, y=250)
+        else:
+            print("File not exists")
 
 
 def submit():
@@ -226,8 +367,13 @@ def getting_url():
     return url.get()
 
 
-Check_Folder.check_folder_video()
-Check_Folder.check_folder_mp3()
+if os.path.exists("Config\\UserProfile\\ExportPath.ydbi"):
+    path_file = open("Config\\UserProfile\\ExportPath.ydbi", "r")
+    path = path_file.read()
+    Check_Folder.main(path)
+
+else:
+    Check_Folder.main(".")
 
 master = Tk()
 master.geometry("500x400")
@@ -246,9 +392,30 @@ master.resizable(False, False)
 
 # Var
 url = StringVar()
+folder_path = StringVar()
 
+# Title
 label = Label(text="- Youtube Downloader -", bg="white")
 label.place(x=180, y=40)
+
+# Menu Bar
+menu_bar = Menu(master)
+file_menu = Menu(menu_bar, tearoff=0)
+edit_menu = Menu(menu_bar, tearoff=0)
+help_menu = Menu(menu_bar, tearoff=0)
+
+edit_menu.add_command(label="Set Exportation Path", command=save_exportation_path)
+file_menu.add_command(label="Get Actual Exportation Path", command=exportation_information_path)
+file_menu.add_separator()
+file_menu.add_command(label='Exit', command=exit_me)
+
+help_menu.add_command(label="ReadMe", command=read_me)
+help_menu.add_separator()
+help_menu.add_command(label="About", command=about)
+
+menu_bar.add_cascade(label="File", menu=file_menu)
+menu_bar.add_cascade(label="Edit", menu=edit_menu)
+menu_bar.add_cascade(label="Help", menu=help_menu)
 
 # Object TextBox Top Level
 url_box_entry = Entry(master, textvariable=url)
@@ -268,11 +435,5 @@ Downmp3_button = Button(master, text="Download MP3", relief="groove", activeback
                         command=download_mp3)
 Downmp3_button.place(x=380, y=250)
 
-# Sig
-sig = Label(text="Developed By GOlD_C4TTL3M@N", bg="Red3", fg="black")
-sig.place(x=150, y=350)
-
-sig2 = Label(text="Version: 1.0", bg='Red3', fg="black")
-sig2.place(x=210, y=370)
-
+master.config(menu=menu_bar)
 master.mainloop()
